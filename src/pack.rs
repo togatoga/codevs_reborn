4c6c117d38951d5b9697a1c854bb03390befaf06
togatoga <togasakitogatoga@gmail.com>
2019-04-23 00:20:43 +0900

    Add a file pack.rs

---
+++ src/pack.rs
@@ -0,0 +1,29 @@
+type Block = u8;
+#[derive(Debug)]
+pub struct Pack {
+    pub blocks: Vec<Block>,
+}
+impl Pack {
+    pub fn rotate(&mut self)  {
+        let tmp1 = self.blocks[0];
+        let tmp2 = self.blocks[1];
+        self.blocks[0] = self.blocks[2];
+        self.blocks[1] = tmp1;
+        let tmp1 = self.blocks[3];
+        self.blocks[3] = tmp2;
+        self.blocks[2] = tmp1;
+    }
+}
+
+#[test]
+fn test_rotate() {
+    let mut p = Pack {blocks: vec![9, 5, 0, 3]};
+    p.rotate();
+    assert_eq!(p.blocks, vec![0, 9, 3, 5]);
+    p.rotate();
+    assert_eq!(p.blocks, vec![3, 0, 5, 9]);
+    p.rotate();
+    assert_eq!(p.blocks, vec![5, 3, 9, 0]);
+    p.rotate();
+    assert_eq!(p.blocks, vec![9, 5, 0, 3]);
+}
