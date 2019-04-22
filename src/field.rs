3577b96116e8163c84c8acedb62d1fca028e8401
togatoga <togasakitogatoga@gmail.com>
2019-04-23 01:29:24 +0900

    Add a test

--- src/field.rs
+++ src/field.rs
@@ -5,19 +5,43 @@ pub const EMPTY_BLOCK: u8 = 0;
 #[derive(Debug)]
 pub struct Field {
     field: [[u8; FIELD_WIDTH]; FIELD_HEIGHT],
-    heights: [u8; FIELD_WIDTH],
+    bottoms: [u8; FIELD_WIDTH],
 }
 
 
 pub fn new(field: [[u8; FIELD_WIDTH]; FIELD_HEIGHT]) -> Field {
-    let mut heights: [u8; FIELD_WIDTH] = [0; FIELD_WIDTH];
+    let mut bottoms: [u8; FIELD_WIDTH] = [0; FIELD_WIDTH];
     for x in 0..FIELD_WIDTH {
         let mut y = 0;
         while y < FIELD_HEIGHT && field[y][x] == EMPTY_BLOCK {
             y += 1;
         }
-        heights[x] = y as u8;
+        bottoms[x] = y as u8;
     }
-    Field { field, heights }
+    Field { field, bottoms }
 }
 
+
+#[test]
+fn test_bottoms() {
+    let field = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    let field = new(field);
+    assert_eq!(field.bottoms, [16, 15, 14, 13, 12, 11, 10, 9, 8, 7]);
+}
