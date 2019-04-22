91d8c0b305a6fb86ca3859e424f0df376ed0f58c
togatoga <togasakitogatoga@gmail.com>
2019-04-23 00:20:43 +0900

    Add a file pack.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+mod pack;
 use std::io::{StdinLock, Stdin};
+
+
+use crate::pack::Pack;
 
 const MAX_TURN: usize = 500;
 const FIELD_WIDTH: usize = 10;
@@ -6,36 +10,6 @@ const FIELD_HEIGHT: usize = 16;
 
 
 type Field = Vec<Vec<u8>>;
-type Block = u8;
-
-#[derive(Debug)]
-struct Pack {
-    blocks: Vec<Block>,
-}
-impl Pack {
-    pub fn rotate(&mut self)  {
-        let tmp1 = self.blocks[0];
-        let tmp2 = self.blocks[1];
-        self.blocks[0] = self.blocks[2];
-        self.blocks[1] = tmp1;
-        let tmp1 = self.blocks[3];
-        self.blocks[3] = tmp2;
-        self.blocks[2] = tmp1;
-    }
-}
-
-#[test]
-fn test_rotate() {
-    let mut p = Pack {blocks: vec![9, 5, 0, 3]};
-    p.rotate();
-    assert_eq!(p.blocks, vec![0, 9, 3, 5]);
-    p.rotate();
-    assert_eq!(p.blocks, vec![3, 0, 5, 9]);
-    p.rotate();
-    assert_eq!(p.blocks, vec![5, 3, 9, 0]);
-    p.rotate();
-    assert_eq!(p.blocks, vec![9, 5, 0, 3]);
-}
 
 #[derive(Debug)]
 struct GameStatus {
@@ -82,6 +56,10 @@ impl<'a> Solver<'a> {
         assert_eq!(end, "END");
         GameStatus { rest_time_milliseconds, dead_block_count, skill_point, game_score, field }
     }
+
+    pub fn think(&mut self) -> (usize, usize){
+        (0, 0)
+    }
 }
 
 fn solve() {
@@ -96,7 +74,8 @@ fn solve() {
         let player = Solver::read_game_status(&mut sc);
         let enemy = Solver::read_game_status(&mut sc);
         let mut solver = Solver::new(&packs, player, enemy);
-        println!("0 0");
+        let action = solver.think();
+        println!("{} {}", action.0, action.1);
     }
 }
 
