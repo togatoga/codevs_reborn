5a4b07a860450f0e60656634827e110d462cd9a4
togatoga <togasakitogatoga@gmail.com>
2019-04-29 00:51:10 +0900

    Add sub commands to analyze_score.py

--- scripts/analyze_score.py
+++ scripts/analyze_score.py
@@ -8,12 +8,21 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict
 
 
-def read_score_file(file_path):
-    with open(file_path, "r") as f:
-        reader = list(csv.DictReader(f))
-        assert(len(reader) == 1)
-        content = reader[0]
-        return content
+def read_score_files(dir_path, limit):
+    def read_score_file(file_path):
+        with open(file_path, "r") as f:
+            reader = list(csv.DictReader(f))
+            assert(len(reader) == 1)
+            content = reader[0]
+            return content
+    contents = OrderedDict()
+    for idx, file in enumerate(sorted(os.listdir(dir_path))):
+        if idx >= limit:
+            break
+        file_name = os.path.basename(file)
+        file_path = os.path.join(dir_path, file)
+        contents[file_name] = read_score_file(file_path)
+    return contents
 
 
 def analyze_score(cumulative_game_scores, file_path):
@@ -50,21 +59,42 @@ def plot_hist_score(cumulative_game_scores, file_path):
     plt.show()
 
 
-def main(args):
-    root = args.score
-    limit = args.number
-    contents = OrderedDict()
-    # read score files order by the file name
-    for idx, file in enumerate(sorted(os.listdir(root))):
-        if idx >= limit:
-            break
-        file_name = os.path.basename(file)
-        file_path = os.path.join(root, file)
-        contents[file_name] = read_score_file(file_path)
-    # analyze
+def get_scores(contents):
     cumulative_game_scores = []
     for file_name, content in contents.items():
         cumulative_game_scores.append(int(content['cumulative_game_score']))
+    return cumulative_game_scores
+
+
+def battle(args):
+    source = args.source
+    dest = args.dest
+    limit = args.number
+    source_scores = get_scores(read_score_files(source, limit))
+    dest_scores = get_scores(read_score_files(dest, limit))
+    draw = 0
+    win = 0
+    lose = 0
+    for (x, y) in zip(source_scores, dest_scores):
+        if x == y:
+            draw += 1
+        if x < y:
+            win += 1
+        if x > y:
+            lose += 1
+    print(f'Number   : {win + draw + lose}')
+    print(f'Draw     : {draw}')
+    print(f'Dest Win : {win}')
+    print(f'Dest Lose: {lose}')
+
+
+def analyze(args):
+    root = args.score
+    limit = args.number
+    contents = read_score_files(root, limit)
+
+    # analyze
+    cumulative_game_scores = get_scores(contents)
 
     output_path = f'data/analysis/'
     os.makedirs(output_path, exist_ok=True)
@@ -78,8 +108,20 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze score files")
-    parser.add_argument("score", help="score directory path")
-    parser.add_argument(
+    subparsers = parser.add_subparsers(dest="Hogehoge")
+    subparsers.required = True
+
+    analyze_parser = subparsers.add_parser("analyze")
+    analyze_parser.set_defaults(fn=analyze)
+    analyze_parser.add_argument("score", help="score directory path")
+    analyze_parser.add_argument(
+        '-n', '--number', type=int, help="the number of score files which are analyzed", default=500)
+
+    battle_parser = subparsers.add_parser("battle")
+    battle_parser.set_defaults(fn=battle)
+    battle_parser.add_argument("source", help="source directory path")
+    battle_parser.add_argument("dest", help="dest directory path")
+    battle_parser.add_argument(
         '-n', '--number', type=int, help="the number of score files which are analyzed", default=500)
     args = parser.parse_args()
-    sys.exit(main(args))
+    sys.exit(args.fn(args))
