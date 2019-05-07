2afc165e8e559da56354657c96eeead6612cc99d
togatoga <togasakitogatoga@gmail.com>
2019-05-07 22:49:11 +0900

    Start version 3.5

--- src/solver_config.rs
+++ src/solver_config.rs
@@ -16,7 +16,7 @@ pub const DEFAULT_GAZE_BEAM_DEPTH: usize = 3;
 pub const DEFAULT_GAZE_BEAM_WIDTH: usize = 50;
 
 
-pub const SOLVER_VERSION: &str = "3.4";
+pub const SOLVER_VERSION: &str = "3.5";
 impl SolverConfig {
     pub fn default() -> SolverConfig {
         SolverConfig {
